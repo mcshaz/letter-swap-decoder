d5b791f9222808326c2e66a9931cf93bd97ea58f
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2022-10-30 10:37:59 +1300

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,9 @@
-@echo off
-
+echo on
 npm "run" "build"
 cd "dist"
 git "init"
 git "checkout" "-B" "main"
 git "add" "-A"
 git "commit" "-m" "deploy"
-git "push" "-f" "git@github.com:mcshaz\mcshaz\letter-swap-decoder.git" "main:gh-pages"
+git "push" "-f" "git@github.com:mcshaz/letter-swap-decoder.git" "main:gh-pages"
 cd ..
